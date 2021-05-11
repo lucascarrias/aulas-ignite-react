b8268e1c7d119e4cf5e010e7b9604c9d8ea16926
Lucas Carrias <lucascarrias@outlook.com>
2021-05-10 21:00:58 -0300

    Chapter 2 - Create Header component

---
+++ 02-moneycare/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="MoneyCare" />
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  );
+}
