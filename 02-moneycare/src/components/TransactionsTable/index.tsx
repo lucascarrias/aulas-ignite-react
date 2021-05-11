9b431acbd495564c469d0c9a39e4760d33b65ea9
Lucas Carrias <lucascarrias@outlook.com>
2021-05-11 09:33:54 -0300

    Add fake api server and create api client serivce

--- 02-moneycare/src/components/TransactionsTable/index.tsx
+++ 02-moneycare/src/components/TransactionsTable/index.tsx
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api.get("transactions")
+      .then((response) => console.log(response.data));
+  }, []);
+
   return (
     <Container>
       <table>
         <thead>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
         </thead>
 
         <tbody>
