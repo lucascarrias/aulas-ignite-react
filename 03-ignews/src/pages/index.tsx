0d474e70ac96e3e53effc12abd1baf2dedd8e73e
Lucas Carrias <lucascarrias@outlook.com>
2021-05-29 08:34:56 -0300

    Chapter 3 - Setup typescript, document and global styling.

---
+++ 03-ignews/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1>
+        Hello <span>World</span>
+      </h1>
+    </>
+  );
+}
