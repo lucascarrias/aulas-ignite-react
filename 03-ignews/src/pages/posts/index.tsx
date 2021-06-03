39df514fb24111bce6a6368c5f015a29071c9b99
Lucas Carrias <lucascarrias@outlook.com>
2021-06-03 15:50:35 -0300

    Chapter 3 - Create prismic service and list posts

--- 03-ignews/src/pages/posts/index.tsx
+++ 03-ignews/src/pages/posts/index.tsx
@@ -1,8 +1,22 @@
-import Head from 'next/head';
-import React from 'react';
-import styles from './styles.module.scss';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import React from "react";
+import { getPrismicClient } from "../../services/prismic";
+import styles from "./styles.module.scss";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -11,23 +25,48 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>3rd June 2014</time>
-            <strong>My first title</strong>
-            <p>When testing requests/controllers it’s common to have scenarios where a unique request can modify more than one table. For tests like this, chaining expectations will speed up the tests.</p>
-          </a>
-          <a href="#">
-            <time>3rd June 2014</time>
-            <strong>My first title</strong>
-            <p>When testing requests/controllers it’s common to have scenarios where a unique request can modify more than one table. For tests like this, chaining expectations will speed up the tests.</p>
-          </a>
-          <a href="#">
-            <time>3rd June 2014</time>
-            <strong>My first title</strong>
-            <p>When testing requests/controllers it’s common to have scenarios where a unique request can modify more than one table. For tests like this, chaining expectations will speed up the tests.</p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "publication")],
+    { fetch: ["publication.title", "publication.content"], pageSize: 100 }
+  );
+
+  console.log(JSON.stringify(response, null, 2));
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "en-US",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
