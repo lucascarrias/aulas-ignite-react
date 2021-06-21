2d7233fee9b83bd6d0d2253fdb3a127ac07c8c5d
Lucas Carrias <lucascarrias@outlook.com>
2021-06-20 22:26:37 -0300

    Chapter 04 - Create services and paginate mirage responses

--- 04 - dashgo/src/pages/users/index.tsx
+++ 04 - dashgo/src/pages/users/index.tsx
@@ -21,34 +21,10 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { useQuery } from "react-query";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
-    "users",
-    async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5,
-    }
-  );
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -68,6 +44,7 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
             </Heading>
 
             <Link href="/users/create" passHref>
--- 04 - dashgo/src/services/mirage/index.ts
+++ 04 - dashgo/src/services/mirage/index.ts
@@ -1,5 +1,5 @@
-import { createServer, Factory, Model } from "miragejs";
-import faker from 'faker';
+import { createServer, Factory, Model, Response } from "miragejs";
+import faker from "faker";
 
 type User = {
   name: string;
@@ -15,24 +15,38 @@ export function makeServer() {
     factories: {
       user: Factory.extend({
         name(i: number) {
-          return `User ${i + 1}`
+          return `User ${i + 1}`;
         },
         email() {
           return faker.internet.email().toLowerCase();
         },
         createdAt() {
           return faker.date.recent(10);
-        }
+        },
       }),
     },
     seeds(server) {
-      server.createList('user', 10);
+      server.createList("user", 200);
     },
     routes() {
       this.namespace = "api";
       this.timing = 750;
 
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, {'x-total-count': String(total)}, { users })
+      });
       this.post("/users");
 
       this.namespace = "";
